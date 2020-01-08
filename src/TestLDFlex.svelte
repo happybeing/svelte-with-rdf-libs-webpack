ef5204e9d9f9d1a71577bd6072971b4fb5f9b54e
Mark <git@happybeing.com>
2020-01-08 16:58:11 +0000

    Use LDFlex/Comunica to get/list profile info (part working)

--- src/TestLDFlex.svelte
+++ src/TestLDFlex.svelte
@@ -14,15 +14,40 @@ const context = {
   }
 };
 
-console.log('Loading Comunica');
+console.log('Comunica...');
 // The query engine and its source
 const queryEngine = new ComunicaEngine('https://ruben.verborgh.org/profile/');
 
-console.log('Loading LDFlex');
+console.log('LDFlex...');
 // The object that can create new paths
 const path = new PathFactory({ context, queryEngine });
 
+const ruben = path.create({ subject: namedNode('https://ruben.verborgh.org/profile/#me') });
+showPerson(ruben);
+
+async function showPerson(person) {
+  console.log(`This person is ${await person.name}`);
+
+  console.log(`${await person.givenName} is interested in:`);
+  for await (const name of person.interest.label)
+    console.log(`- ${name}`);
+
+  console.log(`${await person.givenName} is friends with:`);
+  for await (const name of person.friends.givenName)
+    console.log(`- ${name}`);
+}
+
 </script>
 
-<div><h2>Rdflib OK</h2>
+<div><h2>Comunica and LDFlex</h2>
+{#await ruben.name}
+  <p>Comunica loading...</p>
+{:then}
+  <p>Comunica done. LDFlex...</p>
+  <!--  I'm not sure how to get ruben.name here.
+        It shows undefined despite above #await! -->
+  <p>ruben: {ruben} and ruben.name: {ruben.name}</p>
+{:catch error}
+  <p style="color: red">{error.message}</p>
+{/await}
 </div>
