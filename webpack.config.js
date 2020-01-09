6af906d6e181591bce69f0e4e6eab41aeae97a97
Mark <git@happybeing.com>
2020-01-09 14:37:30 +0000

    Add babel loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// const BabelEnginePlugin = require('babel-engine-plugin');
 const path = require('path');
 
 const mode = process.env.NODE_ENV || 'development';
@@ -47,14 +48,24 @@ module.exports = {
 					prod ? MiniCssExtractPlugin.loader : 'style-loader',
 					'css-loader'
 				]
-			}
-		]
+			},
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-object-rest-spread']
+          }
+        }
+      }		]
 	},
 	mode,
 	plugins: [
 		new MiniCssExtractPlugin({
 			filename: '[name].css'
-		})
+    })
 	],
   devtool: '#source-map', // #eval-source-map doesn't emit a map!?
 	// devtool: prod ? false: 'source-map'
