703adaef69a1d4fa9bd184d10192b8e8e1aeb124
Mark <git@happybeing.com>
2020-01-09 18:20:53 +0000

    Remove 'browser-list', babel-loader and dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -48,18 +48,7 @@ module.exports = {
 					prod ? MiniCssExtractPlugin.loader : 'style-loader',
 					'css-loader'
 				]
-			},
-      {
-        test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-            plugins: ['@babel/plugin-proposal-object-rest-spread']
-          }
-        }
-      }		]
+			}		]
 	},
 	mode,
 	plugins: [
