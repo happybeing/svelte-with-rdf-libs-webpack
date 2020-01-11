1b6e13f78db2f4e0b1ae9be5e12c0cf4a5037bd5
Mark <git@happybeing.com>
2020-01-11 20:33:29 +0000

    Include comments with the build size overhead for each RDF library

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,8 @@
 <script>
-import TestRdflib from "./TestRdflib.svelte";
-import TestLDFlex from "./TestLDFlex.svelte";
-import TestGraphy from "./TestGraphy.svelte";
-import TestRdfjs from "./TestRdfjs.svelte";
+import TestRdfjs from "./TestRdfjs.svelte"; // Adds 260K to bundle.js 1.4M to bundle.js.map
+import TestGraphy from "./TestGraphy.svelte"; // Adds 360K to bundle.js 1.5M to bundle.js.map (using only the Turtle reader)
+import TestRdflib from "./TestRdflib.svelte"; // Adds 606K to bundle.js 2.6M to bundle.js.map
+import TestLDFlex from "./TestLDFlex.svelte"; // Adds 1.4M to bundle.js 5.3M to bundle.js.map
 
 </script>
 
